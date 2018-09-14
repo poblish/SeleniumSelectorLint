37f98f0deeefb0f59c071af3ea0694595679ca80
Andrew Regan <aregan@gmail.com>
2018-09-15 00:52:42 +0100

    Add equals / hashCode

--- src/main/java/org/hiatusuk/selectorLint/tree/NodeRelation.java
+++ src/main/java/org/hiatusuk/selectorLint/tree/NodeRelation.java
@@ -2,6 +2,8 @@ package org.hiatusuk.selectorLint.tree;
 
 import com.google.common.base.MoreObjects;
 
+import java.util.Objects;
+
 public class NodeRelation {
     private final boolean directParent;
     private final Node target;
@@ -17,6 +19,26 @@ public class NodeRelation {
 
     public boolean isDirectParent() {
         return directParent;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(directParent, target);
+    }
+
+    @Override
+    public boolean equals( Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof NodeRelation)) {
+            return false;
+        }
+        final NodeRelation other = (NodeRelation) obj;
+        return directParent == other.directParent && Objects.equals(target, other.target);
     }
 
     @Override
